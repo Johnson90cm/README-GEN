11c0ba281ad3fc74f7720eb41d8ef4caebef303a
Tank <johnson90.cm@gmail.com>
2021-09-26 16:18:08 -0600

    test 5

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ inquirer.prompt(
 .then(({ title, description, installation, usage, credits, license, tests, github, email }) => {
 var template = 
   `
-  [[!License: ${license}](https://img.shields.io/badge/License-MIT-yellow.svg)]
+  [![License: ${license}](https://img.shields.io/badge/License-MIT-yellow.svg)]
 
   # Project Title: ${title}
 
